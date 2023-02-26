a45895e701786707619388c8f0d08680c06d1979
David Hernandez <86691393+CrazyForge38@users.noreply.github.com>
2023-02-26 17:11:46 -0600

    Update Server.py

--- Server.py
+++ Server.py
@@ -4,7 +4,7 @@ import socket
 import threading
 
 HEADER = 64
-PORT = 5050
+PORT = 5051
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
@@ -13,6 +13,26 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+def handle_client_photo(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{addr}] {msg}")
+            file = open("try.jpg", "ab")
+            file.write(msg)
+            conn.send("Msg received".encode(FORMAT))
+
+            file.close()
+    conn.close()
+        
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
 
@@ -29,15 +49,15 @@ def handle_client(conn, addr):
             conn.send("Msg received".encode(FORMAT))
 
     conn.close()
-        
+
 
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr = server.accept()
+        conn, addr = server.accept() #waits for a connection
         thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
+        thread.start() 
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
