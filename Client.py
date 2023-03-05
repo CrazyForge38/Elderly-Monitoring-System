965a0c93132d1ab96ad3498dc19b74b9ac5c6e0c
David Hernandez <86691393+CrazyForge38@users.noreply.github.com>
2023-03-05 17:32:36 -0600

    Update Client.py
    this is now handling system calls. My main issue was the photo method. Turns out it will not work unless the server has a empty or creates a new file to store said data in

--- Client.py
+++ Client.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+from pickle import TRUE
 import socket
+import sys
 
 HEADER = 64
-PORT = 5051
+PORT = 5053
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -12,7 +14,20 @@ print(SERVER)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def send(msg):
+def send_message(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(f"[Sending] {message.decode(FORMAT)}")
+
+def recv_message():
+    print(f"[clinet ID: #] {client.recv(2048).decode(FORMAT)}")
+    #print(client.recv(2048).decode(FORMAT))
+
+def send_Raw_Data(msg):
     if type(msg) is bytes:
         message = msg
         msg_length = len(message)
@@ -20,26 +35,35 @@ def send(msg):
         send_length += b' ' * (HEADER - len(send_length))
         client.send(send_length)
         client.sendall(msg)
-    else:
-        print(msg)
-        message = msg.encode(FORMAT)
-        msg_length = len(message)
-        send_length = str(msg_length).encode(FORMAT)
-        send_length += b' ' * (HEADER - len(send_length))
-        client.send(send_length)
-        client.send(message)
-    print(client.recv(2048).decode(FORMAT))
- 
-#file = open("thatonephoto.jpg", 'rb')
-#image_data = file.read(2048)
-#while image_data:
-#    send(image_data)
-#    image_data = file.read(2048)
-#file.close()   
-input() 
-send("Hello World!")
-input()
-send("Hello everyone")
-input()
-#send("hello Tim")
-#send(DISCONNECT_MESSAGE) 
+        print(client.recv(2048).decode(FORMAT))
+
+def Sys_Call_Photo():
+    print("#Testing print")
+    file = open("testimage.PNG", 'rb')
+    image_data = file.read(2048)
+    #print(image_data)
+    print()
+    print()
+    while image_data:
+        send_Raw_Data(image_data)
+        image_data = file.read(2048)
+    file.close() 
+    print("[+] closed File")
+    send_message(DISCONNECT_MESSAGE)
+
+def Sys_Call_Request():
+    while TRUE:
+        recv_message()
+        print("Enter a sys call: ", end='')
+        #read = input()
+        sys_call = input()
+        send_message(sys_call)
+        print("After first msg sent")
+        sys_call = int(sys_call)
+        if sys_call == 1:
+            print("phjto call")
+            Sys_Call_Photo()
+        if sys_call == 2:
+            send_message("Test the world")
+
+Sys_Call_Request()
