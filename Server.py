e8117b830b50db657aa10ccca0b01e647b16c88a
David Hernandez <86691393+CrazyForge38@users.noreply.github.com>
2023-03-05 17:34:14 -0600

    Update Server.py
    handling system calls and had trouble with the photo def due to reasons. As for the DISCONNECT_MESSAGE, I have to encode it to not show up with b'' around it even though I am sending it through my send_message def in the client

--- Server.py
+++ Server.py
@@ -1,62 +1,95 @@
 #!/usr/bin/python3
 
-import socket 
+#from asyncio.windows_events import NULL
+#from pickle import FALSE, TRUE
+from ctypes.wintypes import BYTE
+from pickle import TRUE
+import socket
+#import sys 
 import threading
 
 HEADER = 64
-PORT = 5051
+PORT = 5053
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+EMPTY_BYTE = "b'!DISCONNECT'"
+
+client_counter = 0
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 def handle_client_photo(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+    #print(f"[NEW CONNECTION] {addr} connected.")
+    print("Hanbdling photo")
 
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER)
+        print("[+] in photo while loop")
+        msg_length = conn.recv(HEADER)#
         if msg_length:
             msg_length = int(msg_length)
-            msg = conn.recv(msg_length)
-            if msg == DISCONNECT_MESSAGE:
+            msg = conn.recv(msg_length)#
+            if msg == DISCONNECT_MESSAGE.encode(FORMAT):
                 connected = False
+                print(f"[{addr}] Client disconnected")
+                #conn.close()
+                return 
 
             print(f"[{addr}] {msg}")
-            file = open("try.jpg", "ab")
+            file = open("try.PNG", "ab")
             file.write(msg)
             conn.send("Msg received".encode(FORMAT))
-
             file.close()
     conn.close()
-        
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
+    return
 
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
 
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
+def handling_client(conn, addr): # this whole thing in a while loop
+    print("We are waiting for the client to send us commands")
+    while TRUE:
+        send_message("Waiting for a sys call", conn, addr)
+        sys_call = recv_message(conn, addr)
+        sys_call = int(sys_call)
+        #check to see if its a numner, if not a loop
 
-    conn.close()
+        if sys_call == 1:
+            print(f"[{addr}] sys_call1: {sys_call}")
+            handle_client_photo(conn, addr)
+        if sys_call == 2:
+            print(f"[{addr}] sys_call2: {sys_call}")
+            recv_message(conn, addr)
+        if sys_call == 3:
+            print(f"[{addr}] sys_call3: {sys_call}")
+        if sys_call == 4:
+            print("wrong")
 
+
+def send_message(msg, conn, addr): #no need fot addr
+    conn.send(msg.encode(FORMAT))
+    print(f"[{addr}] {msg}")
+
+def recv_message(conn, addr):
+    print("[+] Recieving message", end='')
+    msg_length = conn.recv(HEADER).decode(FORMAT)
+    if msg_length:
+         msg_length = int(msg_length)
+         msg = conn.recv(msg_length).decode(FORMAT)
+         if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+         print(f"[{addr}] {msg}")
+    return msg
 
 def start():
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr = server.accept() #waits for a connection
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        conn, addr = server.accept() #waits for a connection and this is where I should do the nnummber thing  
+        thread = threading.Thread(target=handling_client, args=(conn, addr))
         thread.start() 
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
