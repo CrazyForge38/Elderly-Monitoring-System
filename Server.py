85ad28965911fcd760b071e47fdf1b8240fb8b4e
David Hernandez <86691393+CrazyForge38@users.noreply.github.com>
2023-03-15 18:19:02 -0500

    Update Server.py
    fixed everything except handling multiple clients, for some reason it doesnt work any more

--- Server.py
+++ Server.py
@@ -7,12 +7,12 @@ from pickle import TRUE
 import socket
 #import sys 
 import threading
-import sys
 import subprocess
 import json
+import os
 
 HEADER = 64
-PORT = 5051
+PORT = 5053
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
@@ -25,24 +25,18 @@ client_counter = 0
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def handle_client_photo(conn, addr):
-    #print(f"[NEW CONNECTION] {addr} connected.")
-    print("Hanbdling file")
-
+def handle_client_photo(conn, addr): ##3 does not get called
     connected = True
     while connected:
-        print("[+] in while loop")
         msg_length = conn.recv(HEADER)#
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length)#
             if msg == DISCONNECT_MESSAGE.encode(FORMAT):
                 connected = False
-                print(f"[{addr}] Client disconnected")
-                #conn.close()
+                print(f"[{addr}] file recived")
                 return 
 
-            print(f"[{addr}] {msg}")
             file = open("calc.ods", "ab")
             file.write(msg)
             conn.send("Msg received".encode(FORMAT))
@@ -50,88 +44,97 @@ def handle_client_photo(conn, addr):
     conn.close()
     return
 
-def grab_Json(conn, addr, id):
-    dir_name = "mainboard" + str(id)
-    subprocess.run(['mkdir', dir_name], stdout=subprocess.PIPE)
-    subprocess.run(['cd', dir_name], stdout=subprocess.PIPE)
-    file = open("Json.txt", "w") ### I will need a mutex!!!!
+def read_json(conn, addr, board_path):
+    file_path = os.path.join(board_path, "json.txt")
+    with open(file_path) as f:
+        json_metadata = json.load(f)
+        metadata = json_metadata
+        return metadata
+
+def recv_local_json(conn, addr, board_path):
+    file_path = os.path.join(board_path, "json.txt")
+    file = open(file_path, "w") ### I will need a mutex!!!!
     file.close()
-    #print(f"[NEW CONNECTION] {addr} connected.")
-    print("Hanbdling file")
 
     connected = True
     while connected:
-        print("[+] in while loop")
         msg_length = conn.recv(HEADER)#
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length)#
             if msg == DISCONNECT_MESSAGE.encode(FORMAT):
                 connected = False
-                print(f"[{addr}] Client disconnected")
-                #conn.close()
+                print(f"[{addr}] file recived")
                 return 
-
-            print(f"[{addr}] {msg}")
-            file = open("Json.txt", "ab")
+            file = open(file_path, "ab")
             file.write(msg)
             conn.send("Msg received".encode(FORMAT))
             file.close()
     conn.close()
     return
 
-def grab_Storage_Data(conn, addr):
-    file_type = "EMPTY"
-    free = "EMPTY"
-    instruction = 0
-    print("+_+_+_+_+_")
-    output = subprocess.run(['python3', '/home/david/Desktop/Backend/jrw.py', file_type, str(instruction), free], stdout=subprocess.PIPE)
-    print("///////////////////////////////")
-    #metadata = json.loads(output.stdout.decode('utf-8'))
-    print(f" this is the return output: {output}")
-    print("[+]")
-    print("[+]")
-    print("[+]")
-    print("tetstetstesetsettse")
-    print("[++]")
-    print("[++]")
-    print("[++]")
-    print(output)
-    print("[+++]")
-    print("[+++]")
-    print("[+++]")
-    stdout_str = output.stdout.decode('utf-8')
-    print(stdout_str, end='')  # print the contents of stdout_str to the console
-    print("[++++]")
-    print("[++++]")
-    print("[++++]")
-    #location = stdout_str['location']
-    metadata = json.loads(stdout_str)
-    file_type = metadata['location']
-    location = stdout_str['location']
-    print(file_type, location)
+def handle_client_file(conn, addr, file_path):
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER)#
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length)#
+            if msg == DISCONNECT_MESSAGE.encode(FORMAT):
+                connected = False
+                print(f"[{addr}] file recived")
+                return 
+            file = open(file_path, "ab")
+            file.write(msg)
+            conn.send("Msg received".encode(FORMAT))
+            file.close()
+    conn.close()
+    return
 
-def handling_client(conn, addr): # this whole thing in a while loop
-    # code to create directory of id x
-    print("We are waiting for the client to send us commands")
+def set_and_read_file(conn, addr, board_path, metadata):
+    file_type = metadata['file_type'] # need to append path to it
+    path = str(board_path) + str(file_type)
+    file_path = os.path.join(board_path, file_type)
+    handle_client_file(conn, addr, file_path)
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER)#
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length)#
+            if msg == DISCONNECT_MESSAGE.encode(FORMAT):
+                connected = False
+                print(f"[{addr}] file recived")
+                return 
+            file = open(file_path, "ab")
+            file.write(msg)
+            conn.send("Msg received".encode(FORMAT))
+            file.close()
+    conn.close()
+    return
+
+def handling_client(conn, addr, id): # this whole thing in a while loop
+    dir_name = "mainboard" + str(id)
+    generic_file_path = "/home/david/Desktop/Backend/"
+    board_path = str(generic_file_path) + (dir_name) # board specific path
+
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+        print(f"are we making a directory {dir_name}")
+
+    print(f"We are waiting for the client{id} to send us commands")
     while TRUE:
         send_message("Waiting for a sys call", conn, addr)
         sys_call = recv_message(conn, addr)
         sys_call = int(sys_call)
-        #check to see if its a numner, if not a loop
 
-        if sys_call == 1:
-            print(f"[{addr}] sys_call1: {sys_call}")
-            handle_client_photo(conn, addr)
+        if sys_call == 1: # getting file
+            recv_local_json(conn, addr, board_path)
+            metadata = read_json(conn, addr, board_path)
+            set_and_read_file(conn, addr, board_path, metadata)
         if sys_call == 2:
-            print(f"[{addr}] sys_call2: {sys_call}")
             recv_message(conn, addr)
-        if sys_call == 3:
-            print(f"[{addr}] sys_call3: {sys_call}")
-        if sys_call == 4:
-            grab_Json(conn, addr, id) ## need to store this in another directory. 
-            #grab_Storage_Data(conn, addr)
-
 
 def send_message(msg, conn, addr): #no need fot addr
     conn.send(msg.encode(FORMAT))
@@ -156,9 +159,9 @@ def start():
     while True:
         id =+ 1
         conn, addr = server.accept() #waits for a connection and this is where I should do the nnummber thing  
-        thread = threading.Thread(target=handling_client, args=(conn, addr))
+        thread = threading.Thread(target=handling_client, args=(conn, addr, threading.active_count() - 1))
         thread.start() 
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 
 print("[STARTING] server is starting...")
