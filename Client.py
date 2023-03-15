4a3dd6b95c47e7ed36877bb8a517b067806b67c5
David Hernandez <86691393+CrazyForge38@users.noreply.github.com>
2023-03-15 16:02:02 -0500

    Update Client.py
    updating to send json and then the actual file

--- Client.py
+++ Client.py
@@ -1,85 +1,214 @@
 #!/usr/bin/python3
+
+#from asyncio.windows_events import NULL
+#from pickle import FALSE, TRUE
+#from ctypes.wintypes import BYTE
 from pickle import TRUE
 import socket
-import sys
+#import sys 
+import threading
 import subprocess
 import json
-
+import os
 
 HEADER = 64
 PORT = 5053
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
-print(SERVER)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+EMPTY_BYTE = "b'!DISCONNECT'"
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+client_counter = 0
 
-def send_message(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(f"[Sending] {message.decode(FORMAT)}")
 
-def recv_message():
-    print(f"[clinet ID: #] {client.recv(2048).decode(FORMAT)}")
-    #print(client.recv(2048).decode(FORMAT))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
-def send_Raw_Data(msg):
-    if type(msg) is bytes:
-        message = msg
-        msg_length = len(message)
-        send_length = str(msg_length).encode(FORMAT)
-        send_length += b' ' * (HEADER - len(send_length))
-        client.send(send_length)
-        client.sendall(msg)
-        print(client.recv(2048).decode(FORMAT))
+def handle_client_photo(conn, addr):
+    #print(f"[NEW CONNECTION] {addr} connected.")
+    print("Hanbdling photo")
 
-def Sys_Call_Photo():
-    print("#Testing print")
-    file = open("Json.txt", 'rb')
-    image_data = file.read(2048)
-    #print(image_data)
-    print()
-    print()
-    while image_data:
-        send_Raw_Data(image_data)
-        image_data = file.read(2048)
-    file.close() 
-    print("[+] closed File")
-    send_message(DISCONNECT_MESSAGE)
+    connected = True
+    while connected:
+        print("[+] in photo while loop")
+        msg_length = conn.recv(HEADER)#
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length)#
+            if msg == DISCONNECT_MESSAGE.encode(FORMAT):
+                connected = False
+                print(f"[{addr}] Client disconnected")
+                #conn.close()
+                return 
 
-def update_json():
-    print('+')
-    metadata = {'sensor_id': 122, 'date': 'xx/xx/xx', 'time': '12:23:43', 'Board_id': 32, 'location': 'bedroom', 'path': 'fes/fse/sef', 'file_type': '.ods', 'action': 'add to this'}
-    metadata_str = json.dumps(metadata)
-    file_type = "jsontest.txt"
-    instruction = 1
-    print('+')
-    result = subprocess.run(['python3', '/home/david/Desktop/Backend/jrw.py', file_type, str(instruction), metadata_str], stdout=subprocess.PIPE)
-    print(result.stdout.decode())
-    print(')))))')
+            print(f"[{addr}] {msg}")
+            file = open("calc.ods", "ab")
+            file.write(msg)
+            conn.send("Msg received".encode(FORMAT))
+            file.close()
+    conn.close()
+    return
 
-def Sys_Call_Request():
+def read_json(conn, addr, board_path):
+    file_path = os.path.join(board_path, "json.txt")
+    with open(file_path) as f:
+        #json_metadata = f.readline()
+        json_metadata = json.load(f)
+        metadata = json.loads(json_metadata)
+        print(metadata)
+        return metadata
+        #json_metadata = json.load(f) ### we dont need thisd here, should go into setup
+        sensor_id = metadata['sensor_id']
+        location = metadata['location']
+        file_type = metadata['file_type']
+        print(sensor_id, location, file_type)
+        print("[+] Returning from reading from jrw.py")
+        print("[++++++++++++++++++++++++++++]")
+
+def recv_local_json(conn, addr, board_path):
+    file_path = os.path.join(board_path, "json.txt")
+    file = open(file_path, "w") ### I will need a mutex!!!!
+    file.close()
+    print("Hanbdling file")
+
+    connected = True
+    while connected:
+        print("[+] in while loop")
+        msg_length = conn.recv(HEADER)#
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length)#
+            if msg == DISCONNECT_MESSAGE.encode(FORMAT):
+                connected = False
+                print(f"[{addr}] Client disconnected")
+                #conn.close()
+                return 
+
+            print(f"[{addr}] {msg}")
+            file = open(file_path, "ab")
+            file.write(msg)
+            conn.send("Msg received".encode(FORMAT))
+            file.close()
+    conn.close()
+    return
+
+def handle_client_photo(conn, addr):
+    #print(f"[NEW CONNECTION] {addr} connected.")
+    print("Hanbdling photo")
+
+    connected = True
+    while connected:
+        print("[+] in photo while loop")
+        msg_length = conn.recv(HEADER)#
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length)#
+            if msg == DISCONNECT_MESSAGE.encode(FORMAT):
+                connected = False
+                print(f"[{addr}] Client disconnected")
+                #conn.close()
+                return 
+
+            print(f"[{addr}] {msg}")
+            file = open("calc.ods", "ab")
+            file.write(msg)
+            conn.send("Msg received".encode(FORMAT))
+            file.close()
+    conn.close()
+    return
+
+def set_and_read_file(conn, addr, board_path, metadata):
+    print("@@@@@@@@@@@@@@@@@@@@@@@@")
+    print(metadata)
+    file_type = metadata['file_type']
+    print("##########################")
+    print(file_type)
+    recv_message(conn, addr)
+    connected = True
+    while connected:
+        print("[+] in file tpoye area")
+        msg_length = conn.recv(HEADER)#
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length)#
+            if msg == DISCONNECT_MESSAGE.encode(FORMAT):
+                connected = False
+                print(f"[{addr}] Client disconnected")
+                #conn.close()
+                return 
+
+            print(f"[{addr}] {msg}")
+            file = open(file_type, "ab")
+            file.write(msg)
+            conn.send("Msg received".encode(FORMAT))
+            file.close()
+    conn.close()
+    return
+
+def handling_client(conn, addr, id): # this whole thing in a while loop
+    dir_name = "mainboard" + str(id)
+    generic_file_path = "/home/david/Desktop/Backend/"
+    board_path = str(generic_file_path) + (dir_name) # board specific path
+    print(board_path)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+        print(f"are we making a directory {dir_name}")
+    print(f"We are waiting for the client{id} to send us commands")
     while TRUE:
-        recv_message()
-        print("Enter a sys call: ", end='')
-        #read = input()
-        sys_call = input()
-        send_message(sys_call)
-        print("After first msg sent")
+        send_message("Waiting for a sys call", conn, addr)
+        sys_call = recv_message(conn, addr)
         sys_call = int(sys_call)
-        if sys_call == 1:
-            print("phjto call")
-            Sys_Call_Photo()
-        if sys_call == 2:
-            send_message("Test the world")
-        if sys_call == 7:
-            update_json()
+        #check to see if its a numner, if not a loop
 
-Sys_Call_Request()
+        if sys_call == 9:
+            print(f"[{addr}] sys_call1: {sys_call}")
+            handle_client_photo(conn, addr)
+        if sys_call == 2:
+            print(f"[{addr}] sys_call2: {sys_call}")
+            recv_message(conn, addr)
+        if sys_call == 3:
+            print(f"[{addr}] sys_call3: {sys_call}")
+        if sys_call == 1: # getting file
+            print(3333)
+            recv_local_json(conn, addr, board_path)
+            metadata = read_json(conn, addr, board_path)
+            print("!!!!!!!!!!!!!!!!!!!")
+            print(metadata)
+            set_and_read_file(conn, addr, board_path, metadata)
+
+            # grab values
+            # set file up and recv file
+
+
+def send_message(msg, conn, addr): #no need fot addr
+    conn.send(msg.encode(FORMAT))
+    print(f"[{addr}] {msg}")
+
+def recv_message(conn, addr):
+    print("[+] Recieving message", end='')
+    msg_length = conn.recv(HEADER).decode(FORMAT)
+    if msg_length:
+         msg_length = int(msg_length)
+         msg = conn.recv(msg_length).decode(FORMAT)
+         if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+         print(f"[{addr}] {msg}")
+    return msg
+
+def start():
+    id = 0
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        id =+ 1
+        conn, addr = server.accept() #waits for a connection and this is where I should do the nnummber thing  
+        thread = threading.Thread(target=handling_client, args=(conn, addr, threading.active_count() - 1))
+        #print(f"[ACTIVE CONNECTIONS] {idount() - 1}")
+        thread.start() 
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
