a342d69aac4df6da8544aedd38a29ada6e843c20
David Hernandez <86691393+CrazyForge38@users.noreply.github.com>
2023-04-12 09:42:12 -0500

    moving to the pi

--- Client.py
+++ Client.py
@@ -7,7 +7,7 @@ import json
 
 
 HEADER = 64
-PORT = 5052
+PORT = 5053
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -24,11 +24,10 @@ def send_message(msg):
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
-    print(f"[Sending] {message.decode(FORMAT)}")
+    print(f"[+][Sending] {message.decode(FORMAT)}")
 
 def recv_message():
-    print(f"[clinet ID: #] {client.recv(2048).decode(FORMAT)}")
-    #print(client.recv(2048).decode(FORMAT))
+    print(f"[+][clinet ID: #] {client.recv(2048).decode(FORMAT)}")
 
 def send_Raw_Data(msg):
     if type(msg) is bytes:
@@ -38,15 +37,11 @@ def send_Raw_Data(msg):
         send_length += b' ' * (HEADER - len(send_length))
         client.send(send_length)
         client.sendall(msg)
-        print(client.recv(2048).decode(FORMAT))
 
-def Sys_Call_Photo(file):
-    print("#Testing print")
-    file = open(file, 'rb')
+def send_file(file):
+    path = '/home/pi/Desktop/Backend/' + file
+    file = open(path, 'rb')
     image_data = file.read(2048)
-    #print(image_data)
-    print()
-    print()
     while image_data:
         send_Raw_Data(image_data)
         image_data = file.read(2048)
@@ -54,33 +49,30 @@ def Sys_Call_Photo(file):
     print("[+] closed File")
     send_message(DISCONNECT_MESSAGE)
 
-def update_json():
-    print('+')
-    metadata = {'sensor_id': 122, 'date': 'xx/xx/xx', 'time': '12:23:43', 'Board_id': 32, 'location': 'bedroom', 'path': 'fes/fse/sef', 'file_type': '.ods', 'action': 'add to this'}
-    metadata_str = json.dumps(metadata)
-    file_type = "jsontest.txt"
-    instruction = 1
-    print('+')
-    result = subprocess.run(['python3', '/home/david/Desktop/Backend/jrw.py', file_type, str(instruction), metadata_str], stdout=subprocess.PIPE)
-    print(result.stdout.decode())
-    print(')))))')
+def update_json(): # some of the metadata will be board specific
+    metadata = {'sensor_id': 78787, 'date': 'xx/xx/xx', 'time': '12:23:43', 'Board_id': 32, 'location': 'bedroom', 'path': 'fes/fse/sef', 'file_type': 'excel1.ods', 'action': 'add to this'}
+    with open('/home/pi/Desktop/Backend/Json.txt', 'r+') as f:
+            f.truncate()
+            f.seek(0)
+            json.dump(metadata, f)
+            print("[+] updated Json")
 
 def Sys_Call_Request():
     while TRUE:
         recv_message()
         print("Enter a sys call: ", end='')
-        #read = input()
         sys_call = input()
         send_message(sys_call)
-        print("After first msg sent")
         sys_call = int(sys_call)
+
         if sys_call == 1:
-            print("phjto call")
-            Sys_Call_Photo("Json.txt")
-            Sys_Call_Photo("calc.ods")
-        if sys_call == 2:
-            send_message("Test the world")
-        if sys_call == 7:
             update_json()
+            print("[+]sending jsona and file")
+            send_file("Json.txt")
+            send_file("calc.ods")
+        if sys_call == 2:
+            user_input = input("[+]Enter a message: ")
+            send_message(user_input)
+
 
 Sys_Call_Request()
